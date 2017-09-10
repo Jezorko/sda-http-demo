3e4834e638e75145a8483836aeda0138655e0c5b
pfiuk <pawel.fiuk@upaid.pl>
2017-09-10 10:45:06 +0200

    Project initialization

---
+++ src/main/java/pl/sdacademy/HttpDemoApplication.java
@@ -0,0 +1,12 @@
+package pl.sdacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpDemoApplication.class, args);
+	}
+}
