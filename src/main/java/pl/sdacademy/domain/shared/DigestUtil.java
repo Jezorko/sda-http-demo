3bea3a9c07e86ab5ead2ce2b465a176e80a2983d
pfiuk <pawel.fiuk@upaid.pl>
2017-09-16 14:41:46 +0200

    Added randomized salting for passwords

--- src/main/java/pl/sdacademy/domain/shared/DigestUtil.java
+++ src/main/java/pl/sdacademy/domain/shared/DigestUtil.java
@@ -3,10 +3,14 @@ package pl.sdacademy.domain.shared;
 import org.springframework.stereotype.Component;
 
 import static org.apache.commons.codec.digest.DigestUtils.sha256Hex;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
 
 @Component
 public class DigestUtil {
+
+    private static final String RANDOM_SALT = randomAlphanumeric(10);
+
     public String generateHashFrom(String password) {
-        return sha256Hex(password);
+        return sha256Hex(sha256Hex(password) + RANDOM_SALT);
     }
 }
